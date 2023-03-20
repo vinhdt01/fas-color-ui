da43c98bc884e183255637485cb35c8b755ea991
vinh <daothevinh.tv@gmail.com>
2023-03-20 23:04:13 +0700

    update

--- src/framework/rest/ssr/get-product.ts
+++ src/framework/rest/ssr/get-product.ts
@@ -1,7 +1,7 @@
 export async function getServerSideProps() {
-  let base_url:string = process.env.BASE_URL;
+  let base_url = process.env.BASE_URL;
 
-  let res = await fetch(`${base_url}/product`)
+  let res = await fetch(`${base_url}` + '/product')
   let data = await res.json()
   return { props: { data } }
 
